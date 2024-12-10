92bbfd7e8d1c6f23c5b103066a32ed3f2970bcd1
Hugo Crd <hugocordi2003@gmail.com>
2024-12-10 10:01:51 +0100

    Add files via upload

--- gettftp.c
+++ gettftp.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <netdb.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	////////////////////////////////////////////////// get the host adress, the file from the user and the port number
+	////////////////////////////////////////////////// get the host name, the file from the user and the port number
     if (argc != 4) {
         printf("Error\n");
         return 1;
@@ -14,7 +15,35 @@ int main(int argc, char *argv[]) {
     printf("Host: %s, File: %s, Port %s\n", host, file, port);
     
     ///////////////////////////////////////////////// get the IP adress of the host
-   
-	}
+    
+	struct addrinfo hints;     
+    struct addrinfo *res, *r;  
+    int e;
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+
+    // Use of getaddrinfo
+    e = getaddrinfo(argv[1], NULL, &hints, &res);
+    
+    if (e != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(e));
+        exit(EXIT_FAILURE);
+    }
+
+    // Show the results
+    printf("Results for the host name : %s\n", argv[1]);
+    for (r = res; r != NULL; r = r->ai_next) {
+        printf("Family = %d, Type = %d, Protocol = %d\n",
+               r->ai_family, r->ai_socktype, r->ai_protocol);
+               
+    }
+      
+    
+    // Free the memory
+    freeaddrinfo(res);
+    exit(EXIT_SUCCESS);
+
+	return 0; 
 	
 }
+	
